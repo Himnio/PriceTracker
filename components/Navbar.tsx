b18e4fd265e20a284f9214fe11c5a9d07dc7dcd2
Hirak Medi <hirak@DOCQIN153.local>
2023-11-26 13:32:44 +0530

    Added the Navbar,Searchbar,Herocarousel and layout changes

---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const navIcons = [
+  {src: '/assets/icons/search.svg', alt: 'search'},
+  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+  { src: '/assets/icons/user.svg', alt: 'user' },
+]
+
+const Navbar = () => {
+  return (
+   <header className='w-full'>
+    <nav className='nav'>
+      <Link className='flex gap-1 items-center'
+      href={"/"}>
+        <Image
+          src="/assets/icons/logo.svg"
+          width={27}
+          height={27}
+          alt='Logo'
+        />
+        <p className='nav-logo'>
+        Price <span className='text-primary'>Tracker</span>
+        </p>
+      </Link>
+      <div className='flex items-center gap-5'>
+       {navIcons.map((icons)=>(
+        <Image
+          key={icons.alt}
+          alt={icons.alt}
+          src={icons.src}
+          width={28}
+          height={28}
+          className='object-contain'
+        />
+       ))
+       }
+      </div>
+    </nav>
+    </header>
+  )
+}
+export default Navbar
