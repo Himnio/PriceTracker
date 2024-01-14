43e410cfc8640deb89b866861982e2457e63ff1d
Hirak Medi <hirak@DOCQIN153.local>
2024-01-14 11:00:29 +0530

    Added Model Component

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,8 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
@@ -81,7 +83,7 @@ export async function getSimilarProducts(productId: string) {
 
     const similarProducts = await Product.find({
       _id: { $ne: productId },
-    }).limit(3);
+    }).limit(6);
 
     return similarProducts;
   } catch (error) {
@@ -89,24 +91,26 @@ export async function getSimilarProducts(productId: string) {
   }
 }
 
-// export async function addUserEmailToProduct(productId: string, userEmail: string) {
-// try {
-// const product = await Product.findById(productId);
+export async function addUserEmailToProduct(
+  productID: string,
+  userEmail: string
+) {
+  try {
+    const product = await Product.findById(productID);
 
-// if(!product) return;
-
-// const userExists = product.users.some((user: User) => user.email === userEmail);
-
-// if(!userExists) {
-// product.users.push({ email: userEmail });
-
-// await product.save();
-
-// const emailContent = await generateEmailBody(product, "WELCOME");
-
-// await sendEmail(emailContent, [userEmail]);
-// }
-// } catch (error) {
-// console.log(error);
-// }
-// }
+    if (!product) return;
+    const userExists = product.users.some(
+      (user: User) => user.email === userEmail
+    );
+    if (!userExists) {
+      product.users.push({
+        email: userEmail,
+      });
+      await product.save();
+      const emailContent = generateEmailBody(product, "WELCOME");
+      await sendEmail(emailContent, [userEmail]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
